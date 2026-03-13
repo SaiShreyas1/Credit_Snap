437188d91851160dab432ebe0c22d58ba84e49dc
Tejas <gvssrk776@gmail.com>
2026-03-14 00:50:46 +0530

    Edited stud files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Import Layout and Pages
+import StudentLayout from './Pages/stud_layout';
 import StudDashboard from './Pages/stud_Dashboard';
+import StudentCanteens from './Pages/stud_canteens';
 
 export default function App() {
   return (
-    <StudDashboard />
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Wrap the student routes inside the Layout! */}
+        <Route path="/student" element={<StudentLayout />}>
+          
+          {/* This redirects /student to /student/dashboard automatically */}
+          <Route index element={<Navigate to="dashboard" replace />} />
+          
+          {/* These pages load inside the <Outlet /> of the Layout */}
+          <Route path="dashboard" element={<StudDashboard />} />
+          <Route path="canteens" element={<StudentCanteens/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
